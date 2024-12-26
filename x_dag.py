9fc83d2cb45bb5f07e01e0c167994a77e38c22ce
Federico Ferrante <disenowebmdp@gmail.com>
2024-12-26 18:48:48 -0500

    Make sure .env loads on the proper path

--- x_dag.py
+++ x_dag.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 import os
 
 # Loading env file
-
-load_dotenv()
+env_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path=env_path)
 
 # Getting X credentials
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
